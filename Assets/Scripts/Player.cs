cf1fd021346f75e6cd32aa1b11323b1c6070cc4e
charJKL <11600464+charJKL@users.noreply.github.com>
2022-06-13 15:18:49 +0200

    Implement scene bounds for player.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,12 +11,28 @@ public class Player : MonoBehaviour
 		transform.position = new Vector3(0,0,0);
 	}
 	
-	void Update()
+	void FixedUpdate()
 	{
 		float horizontal = Input.GetAxis("Horizontal");
 		float vertical = Input.GetAxis("Vertical");
 		
 		Vector3 direction = new Vector3(horizontal, vertical, 0);
 		transform.Translate(direction * speed * Time.deltaTime);
+	}
+	
+	void LateUpdate()
+	{
+		CheckBounds();
+	}
+	
+	void CheckBounds()
+	{
+		float[] rangeX = {-9.0f, 9.0f};
+		float[] rangeY = {-4.0f, 6.0f};
+
+		float boundInX = Mathf.Clamp(transform.position.x, rangeX[0], rangeX[1]);
+		float boundInY = Mathf.Clamp(transform.position.y, rangeY[0], rangeY[1]);
+		
+		transform.position = new Vector3(boundInX, boundInY, transform.position.z);
 	}
 }
