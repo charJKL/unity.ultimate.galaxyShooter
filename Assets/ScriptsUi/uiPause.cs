6c3ebbbff0f07745fce6871db4e6b1fa8200f4f3
charJKL <11600464+charJKL@users.noreply.github.com>
2022-06-16 21:07:40 +0200

    Remove `Ui` namespace.

--- Assets/ScriptsUi/uiPause.cs
+++ Assets/ScriptsUi/uiPause.cs
@@ -2,41 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Ui
+public class uiPause : MonoBehaviour
 {
-	public class uiPause : MonoBehaviour
-	{
 		[SerializeField] private ManagerGame managerGame;
-		
-		[HideInInspector] private Animator animator;
-		
-		const string ANIMATION_SHOW_PAUSE_MENU_BOOL = "showPauseMenu";
-		
-		private void Awake()
-		{
-			animator = GetComponent<Animator>();
-		}
-		
-		public void Show()
-		{
-			gameObject.SetActive(true);
-			animator.SetBool(ANIMATION_SHOW_PAUSE_MENU_BOOL, true);
-		}
-		
-		public void Hide()
-		{
-			animator.SetBool(ANIMATION_SHOW_PAUSE_MENU_BOOL, false);
-			gameObject.SetActive(false);
-		}
-		
-		public void ClickResume()
-		{
-			managerGame.ResumeGame();
-		}
-		
-		public void ClickBackToMenu()
-		{
-			managerGame.BackToMenu();
-		}
+	
+	[HideInInspector] private Animator animator;
+	
+	const string ANIMATION_SHOW_PAUSE_MENU_BOOL = "showPauseMenu";
+	
+	private void Awake()
+	{
+		animator = GetComponent<Animator>();
+	}
+	
+	public void Show()
+	{
+		gameObject.SetActive(true);
+		animator.SetBool(ANIMATION_SHOW_PAUSE_MENU_BOOL, true);
+	}
+	
+	public void Hide()
+	{
+		animator.SetBool(ANIMATION_SHOW_PAUSE_MENU_BOOL, false);
+		gameObject.SetActive(false);
+	}
+	
+	public void ClickResume()
+	{
+		managerGame.ResumeGame();
+	}
+	
+	public void ClickBackToMenu()
+	{
+		managerGame.BackToMenu();
 	}
 }
